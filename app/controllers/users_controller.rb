2b071213321ace584a3501857a296f28ad193dc4
kuro <independentluvSe1bsthi1fe@gmail.com>
2017-09-02 17:26:26 +0900

    Add User_sing_up`:wq

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+def new
+  @user = User.new
+end
+
+def show
+  @user = User.where(id: params[:id])
+  redirect_to root_path if @user.empty?
+
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to @user
+  else
+    render 'new'
+  end
+end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                      :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :comments
   root to: redirect('/ideas')
   resources :ideas
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
